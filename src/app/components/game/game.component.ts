c8ce5f8024885003265345936d466c205b475c9a
Nick <gvmbrtell@gmail.com>
2019-02-04 17:32:07 -0700

    Bring game into game

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NewGame } from '../new-game/new-game';
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-
   constructor() { }
 
+  game: NewGame;
   ngOnInit() {
+    this.game = JSON.parse(sessionStorage.getItem('game'));
   }
 
 }
